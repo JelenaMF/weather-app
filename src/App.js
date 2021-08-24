b9abcf8ef6a5a689e914a4309eee4727f883203f
Jelena Feliciano <jfel217@gmail.com>
2021-08-24 18:53:55 -0400

    added a fetch data function

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ export default function App() {
   //create two states for longitude and latitude
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position){
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position){
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => console.log(res.json()))
+      
+    }
+    fetchData();
     console.log("Latitude is:", lat);
     console.log("longitude is:", long);
   }, [lat, long]);
