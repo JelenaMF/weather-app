0763222f19213ebb19886fff29616372ef1bd3b3
Jelena Feliciano <jfel217@gmail.com>
2021-08-25 22:32:16 -0400

    getting undefined for URL and API key is it even reading the .env file

--- src/App.js
+++ src/App.js
@@ -13,26 +13,41 @@ export default function App() {
   //create two states for longitude and latitude
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  // const [data, setData] = useState([]);
+  //const [data, setData] = useState([]);
+  console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env.REACT_APP_API_URL);
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position){
+      navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
       .then(res => console.log(res))
       
     }
     fetchData();
-    console.log("Latitude is:", lat);
-    console.log("longitude is:", long);
-  }, [lat, long]);
-
+  }, [lat,long])
+  
   return (
     <div className="App">
-      
+      <header className="header">
+        <h1>The Weather App</h1>
+        <input type="search" className="input" placeholder="Search..." />
+        <button>Search</button>
+      </header>
+      <main>
+        <div className="group">
+          <strong>Location: </strong>
+          <span></span>
+        </div>
+        <div className="group">
+          <strong>Temperature:</strong>
+          <span>c&#176;</span>
+        </div>
+      </main>
+     
     </div>
   );
 }
